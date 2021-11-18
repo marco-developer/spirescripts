ade34bb387504d2b6b551315d87a51c93fa16a08
Marco Antonio Marques <70598892+marco-developer@users.noreply.github.com>
2021-11-17 23:38:41 -0300

    Update start_spire.sh

--- start_spire.sh
+++ start_spire.sh
@@ -1,19 +1,17 @@
-#/bin/bash
-
-# This script aims to show sample interactions with SPIRE Server and Agent.
-# To use run the script and firstly start the spire server, through spire-server management menu.
-
 start_spire_server () {
-# # Start the SPIRE Server as a background process
-echo "Starting spire-server..."
-sleep 1
-./spire-server run -config ../conf/server/server.conf &
-sleep 3
+    # Start the SPIRE Server as a background process
+    echo "Starting spire-server..."
+    sleep 1
+    spire-server run &
+    sleep 2
+}
 
+stop_spire() {
+    kill -9 $(ps -ef | grep "spire-agent" | grep -v grep | awk '{print $2}')
+    kill -9 $(ps -ef | grep "spire-server" | grep -v grep | awk '{print $2}')
 }
 
 reset_spire() {
-    # Stop spire-server and agent processes and delete server data
     kill -9 $(ps -ef | grep "spire-agent" | grep -v grep | awk '{print $2}')
     kill -9 $(ps -ef | grep "spire-server" | grep -v grep | awk '{print $2}')
     rm -rf /spire/.data
@@ -25,7 +23,7 @@ create_spiffeid() {
     echo "Enter the selector:"
     read selector
 
-    ./spire-server entry create \
+    spire-server entry create \
         -parentID spiffe://example.org/host \
         -spiffeID spiffe://example.org/$spiffeid \
         -selector $selector
@@ -33,8 +31,7 @@ create_spiffeid() {
 
 
 oauth2spiffeid() {
-    # Receives a Google OAuth token and create a related SPIFFE-ID.
-    echo "Enter the Google OAuth Token:"
+    echo "Enter the OAuth Token:"
     read oauthtoken
     echo "Enter the selector:"
     read selector
@@ -48,7 +45,7 @@ oauth2spiffeid() {
     read userid < <(echo $tokeninfo | jq -r '.user_id')
     read ttl < <(echo $tokeninfo | jq -r '.expires_in')
 
-    ./spire-server entry create \
+    spire-server entry create \
         -parentID spiffe://example.org/host \
         -ttl $ttl \
         -spiffeID spiffe://example.org/$userid \
@@ -59,20 +56,19 @@ delete_spiffeid() {
     echo "Enter the Entry ID:"
     read entryid
 
-    ./spire-server entry delete \
+    spire-server entry delete \
         -entryID $entryid
 }
 
 list_spiffeids() {
-    ./spire-server entry show
+    spire-server entry show
 }
 
 count_spiffeids() {
-    ./spire-server entry count
+    spire-server entry count
 }
 
 update_spiffeid() {
-# Pode atualizar selector entre outras coisas...
      
 	list_spiffeids	
 
@@ -88,113 +84,78 @@ update_spiffeid() {
 	echo "Enter the new SPIFFE-ID:"
 	read spiffeid
 	
-
 	./spire-server entry update -entryID $entryid -selector $selector -parentID $parentid -spiffeID $spiffeid
 	
  }
 
 # mint_JWT() {
-#     #nao tenho ideia do que faz... gera um jwt para um spiffeid?
+
 # }
 
 # mint_x509() {
-#     #nao tenho ideia do que faz... gera um jwt para um spiffeid?
+
 # }
 
 list_agents() {
-    ./spire-server agent list
+    spire-server agent list
 }
 
 count_agents() {
-    ./spire-server agent count
+    spire-server agent count
 }
 
 generate_jointoken () {
 # Generate a one time Join Token. 
 # Use the -spiffeID option to associate the Join Token with spiffe://example.org/host SPIFFE ID. 
+echo "Enter the SPIFFE-ID:"
+read spiffeid
+
 echo "Generating token..."
 sleep 1
-tmp=$(./spire-server token generate -spiffeID spiffe://example.org/host)
+tmp=$(spire-server token generate -spiffeID spiffe://example.org/$spiffeid)
 echo $tmp
 token=${tmp:7}
 # echo $token >> tokens.lst
-echo "$token sucessfuly generated. Ready to start a new agent."
+echo "Generated token: $token. \n Ready to start a new agent."
 }
 
 start_spire_agent () {
 
-    # Generate a token to a new agent.
     generate_jointoken
     # Start the SPIRE Agent as a background process using the token passed by parameter.
     echo "Starting spire-agent..."
     sleep 1
-    ./spire-agent run -config ../conf/agent/agent.conf -joinToken $token &
+    spire-agent run -joinToken $token &
     sleep 1
     token=''
 }
 
 check_spire_server () {
-    ./spire-server healthcheck
+    spire-server healthcheck
 }
 
-# ban_agent() {
-    # echo "Not implemented. :("
-# }
+ban_agent() {
+    echo "Enter the SPIFFE-ID:"
+    read agentid
+
+    spire-server agent ban -spiffeID $agentid
+}
 
 evict_agent() {
     echo "Enter the SPIFFE-ID:"
     read spiffeid
 
-    ./spire-server agent evict -spiffeID $spiffeid
+    spire-server agent evict -spiffeID $spiffeid
 }
 
 SPIFFEID2JWT() {
 
     # usage:
+    # Bellow is a simple fixed example. Needs to be developed.
     # ./jwt_gen.sh <parent-id> <aat> <spiffe-id> <dpr>
     echo "Not implemented. :("
     
-}
-
-# Simulate the Workload API interaction and retrieve the workload SVID bundle by running the api 
-# subcommand in the agent.
-# echo "Interagindo com API..."
-# sleep 1
-# su -c "spire-agent api fetch x509 " workload
-
-# Execution phase:
-# start_spire_server
-# generate_jointoken
-# start_spire_agent $token
-# list_agents
-# create_spiffeid
-# list_spiffeids
-# delete_spiffeid
-
-mainmenu() {
-    clear
-    echo -ne "
-MAIN MENU
-
- 1) Spire Server
- 2) Spire Agent
- 0) Exit
-Choose an option:  "
-    read -r ans
-    case $ans in
-    1)
-        menu_server
-        mainmenu
-        ;;
-    0)
-        echo "Bye bye."
-        exit 0
-        ;;
-    *)
-        echo "Wrong option."
-        exit 1
-        ;;
-    esac
+    
 }
 
 menu_server() {
@@ -203,28 +164,33 @@ menu_server() {
     entries=$(count_spiffeids)
     clear
     echo -ne "
-SPIRE SERVER
-
-Server status: $status || Number of agents: $agents || Number of registration entries: $entries 
-
-1) Server management
-2) Agents
-3) Registration Entries
-0) Back
-Choose an option:  "
++---------------------------------------------------------+
+                  \033[1mSPIRE SERVER\033[0m
++---------------------------------------------------------+
+ \033[1mServer status:\033[0m $status 
+ \033[1mNumber of agents:\033[0m $agents 
+ \033[1mNumber of registration entries:\033[0m $entries 
++---------------------------------------------------------+ 
+ \033[1m1)\033[0m Server management
+ \033[1m2)\033[0m Agents
+ \033[1m3)\033[0m Registration Entries
+ \033[1m0)\033[0m Back
++---------------------------------------------------------+
+ Choose an option: " 
     read -r ans
     case $ans in
         1)
             menu_server_mgmt
             ;;
-        2)  
+        2)
             menu_server_agents
             ;;
         3)
             menu_server_spiffeid
             ;;
         0)
-            mainmenu
+            echo "Bye bye."
+            exit 0
             ;;
         *)
             echo "Wrong option."
@@ -239,26 +205,35 @@ menu_server_mgmt() {
     entries=$(count_spiffeids)
     clear
     echo -ne "
-SPIRE SERVER MANAGEMENT
-
-Server status: $status || Number of agents: $agents || Number of registration entries: $entries 
-
-1) Start
-2) Stop and reset all
-0) Back
++---------------------------------------------------------+
+               \033[1mSPIRE SERVER MANAGEMENT\033[0m
++---------------------------------------------------------+
+ \033[1mServer status:\033[0m $status 
+ \033[1mNumber of agents:\033[0m $agents 
+ \033[1mNumber of registration entries:\033[0m $entries 
++---------------------------------------------------------+ 
+ \033[1m1)\033[0m Start
+ \033[1m2)\033[0m Stop
+ \033[1m3)\033[0m Stop and reset all
+ \033[1m0)\033[0m Back
++---------------------------------------------------------+
 Choose an option:  "
     read -r ans
     case $ans in
         1)
             start_spire_server
-            echo "Press any key to continue..."
             read
             clear
             menu_server_mgmt
             ;;
         2)
+            stop_spire
+            read
+            clear
+            menu_server_mgmt
+            ;;
+        3)
             reset_spire
-            echo "Press any key to continue..."
             read
             clear
             menu_server_mgmt
@@ -278,43 +253,42 @@ menu_server_agents() {
     entries=$(count_spiffeids)
     clear
     echo -ne "
-SPIRE SERVER AGENTS
-
-Server status: $status || Number of agents: $agents || Number of registration entries: $entries
-Available agent token: $token
-
-1) Start new agent
-2) List agents
-3) Ban agent
-4) Evict agent
-0) Back
++---------------------------------------------------------+
+                \033[1mSPIRE SERVER AGENTS\033[0m
++---------------------------------------------------------+
+ \033[1mServer status:\033[0m $status 
+ \033[1mNumber of agents:\033[0m $agents 
+ \033[1mNumber of registration entries:\033[0m $entries 
++---------------------------------------------------------+ 
+ \033[1m1)\033[0m Start new agent
+ \033[1m2)\033[0m List agents
+ \033[1m3)\033[0m Ban agent
+ \033[1m4)\033[0m Evict agent
+ \033[1m0)\033[0m Back
++---------------------------------------------------------+
 Choose an option:  "
     read -r ans
     case $ans in
         1)
-            start_spire_agent
-            echo "Press any key to continue..."
+            start_spire_agent $token
             read
             clear
             menu_server_agents
             ;;
         2)
             list_agents
-            echo "Press any key to continue..."
             read
             clear
             menu_server_agents
             ;;
         3)
             ban_agents
-            echo "Press any key to continue..."
             read
             clear
             menu_server_agents
             ;;
         4)
             evict_agent
-            echo "Press any key to continue..."
             read
             clear
             menu_server_agents
@@ -334,62 +308,53 @@ menu_server_spiffeid() {
     entries=$(count_spiffeids)
     clear
     echo -ne "
-SPIRE SERVER REGISTRATION ENTRIES
-
-Server status: $status || Number of agents: $agents || Number of registration entries: $entries 
-
-1) Create SPIFFE-ID
-2) List SPIFFE-IDs
-3) Delete SPIFFE-ID
-4) Create OAuth2SPIFFE-ID
-5) Create SPIFFE-ID2JWT
-6) Update SPIFFE-ID
-0) Back
++---------------------------------------------------------+
+                 \033[1mSPIRE SERVER ENTRIES\033[0m
++---------------------------------------------------------+
+ \033[1mServer status:\033[0m $status 
+ \033[1mNumber of agents:\033[0m $agents 
+ \033[1mNumber of registration entries:\033[0m $entries 
++---------------------------------------------------------+ 
+ \033[1m1)\033[0m Create SPIFFE-ID
+ \033[1m2)\033[0m List SPIFFE-IDs
+ \033[1m3)\033[0m Delete SPIFFE-ID
+ \033[1m4)\033[0m Create OAuth2SPIFFE-ID
+ \033[1m5)\033[0m Create SPIFFE-ID2JWT
+ \033[1m0)\033[0m Back
+ +---------------------------------------------------------+
 Choose an option:  "
     read -r ans
     case $ans in
         1)
             create_spiffeid
-            echo "Press any key to continue..."
             read
             clear
             menu_server_spiffeid
             ;;
         2)
             list_spiffeids
-            echo "Press any key to continue..."
             read
             clear
             menu_server_spiffeid
             ;;
         3)
             delete_spiffeid
-            echo "Press any key to continue..."
             read
             clear
             menu_server_spiffeid
             ;;
         4)
             oauth2spiffeid
-            echo "Press any key to continue..."
             read
             clear
             menu_server_spiffeid
             ;;
         5)
             SPIFFEID2JWT
-            echo "Press any key to continue..."
             read
             clear
             menu_server_spiffeid
             ;;
-	6) 
-	    update_spiffeid
-	    echo "Press any key to continue..."
-	    read
-	    clear
-	    menu_server_spiffeid
-	    ;;
         0)
             menu_server
             ;;
@@ -399,4 +364,4 @@ Choose an option:  "
     esac
 }
 
-mainmenu
+menu_server
